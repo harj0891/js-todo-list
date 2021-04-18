d705eb7abfa77dae00cfc5f3c1f3e85c38a74922
Harj <harjinder.chahal@spika.com>
2021-04-18 17:54:39 +0100

    adding webpack

---
+++ src/index.js
@@ -0,0 +1,26 @@
+
+let text = () => {
+    console.log("test");
+}
+
+text();
+
+/* item
+- name
+- description/notes
+- status (pending/done)
+- due date (optional)
+- order (optional) -- can later introduce drag/drop 
+- project (can only belong to 1 project, can't be changed)
+*/
+
+
+/* project
+- name
+- description (optional)
+- progress (in %)
+- due date (optional)
+- order (optional) -- can later introduce drag/drop 
+- priority (optional) -- will change project label color
+- headings (optional) -- to group items 
+*/
